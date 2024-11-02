15779a4ebf6035c7aa99a683acebccee77581a0c
yasmineantille <yasmine.antille@gmail.com>
2024-11-02 12:52:45 +0100

    adds scoopy to quote building

--- srg-challenge/src/components/QuoteBuilding/QuoteBuilding.tsx
+++ srg-challenge/src/components/QuoteBuilding/QuoteBuilding.tsx
@@ -1,40 +1,42 @@
 import React from 'react';
-import { Container, Box, Typography, Grid2 as Grid } from '@mui/material';
+import {Container, Box, Typography, Grid2 as Grid} from '@mui/material';
 import { StyledFlippableCard, SectionTitle, TweetCard, TweetAvatar } from '../StyledComponents/StyledComponents';
 import FlippableCard from '../FlippableCard/FlippableCard';
 import './QuoteBuilding.css';
+import { ReactComponent as Avatar } from '../../assets/avatar.svg';
+
 
 const QuoteBuilding = () => {
     return (
-        <Container maxWidth="md" sx={{ mt: 5, mb: 5 }}>
+        <Container maxWidth="md" sx={{mt: 5, mb: 5}}>
             <Typography variant="h3" align="center" color="primary" gutterBottom>
                 Wie erkennt man eine zuverlässige Quelle?
             </Typography>
 
-            <Box sx={{ my: 4 }}>
+            <Box sx={{my: 4}}>
                 <Grid container spacing={3}>
-                    <Grid size={{xs:12, md:6}}>
+                    <Grid size={{xs: 12, md: 6}}>
                         <FlippableCard
                             title={'Zeitungen & Nachrichtenwebseiten'}
                             icon={'newspaper'}
                             flipsideText={'Oft zuverlässiger, da sie Informationen prüfen.'}
                         />
                     </Grid>
-                    <Grid size={{xs:12, md:6}}>
+                    <Grid size={{xs: 12, md: 6}}>
                         <FlippableCard
                             title={'Soziale Medien'}
                             icon={'connect'}
                             flipsideText={'Informationen sind selten überprüft, besser woanders bestätigen.'}
                         />
                     </Grid>
-                    <Grid size={{xs:12, md:6}}>
+                    <Grid size={{xs: 12, md: 6}}>
                         <FlippableCard
                             title={'Blogs'}
                             icon={'locallibrary'}
                             flipsideText={'Häufig Meinungen, nicht immer überprüft.'}
                         />
                     </Grid>
-                    <Grid size={{xs:12, md:6}} >
+                    <Grid size={{xs: 12, md: 6}}>
                         <FlippableCard
                             title={'Zusätzliche Quellen'}
                             icon={'book'}
@@ -42,17 +44,16 @@ const QuoteBuilding = () => {
                         />
                     </Grid>
                 </Grid>
-                <Box mt={3}>
-                    <StyledFlippableCard>
-                        <Typography variant="h5" color="secondary">
-                            Sprache beachten
-                        </Typography>
-                        <Typography>
-                            Sensationelle Wörter können auf weniger vertrauenswürdige Inhalte hinweisen.
-                        </Typography>
-                    </StyledFlippableCard>
-                </Box>
             </Box>
+            <div className="avatarContainer">
+                <Avatar className="avatar"/>
+                <div className="speech-bubble">
+                    <h4>Sprache beachten</h4>
+                    <p>
+                        Sensationelle Wörter können auf weniger vertrauenswürdige Inhalte hinweisen.
+                    </p>
+                </div>
+            </div>
         </Container>
     );
 };
